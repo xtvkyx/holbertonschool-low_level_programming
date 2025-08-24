b66740586ac9b926f63f97b40c6c20fd1ee4f1f4
“xtvkyx” <ljabli@icloud.com>
2025-08-24 16:00:16 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
-main
-puts("Programming is like building a multilingual puzzle");
-return (0);
+init main (void)
+{                                                                                                                                            puts("Programming is like building a multilingual puzzle");
+return (0);                                                                                                                                                                                                                   }
