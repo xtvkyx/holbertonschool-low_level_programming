ccbf1344a23969e75fe8b5c129d8a385d552c5e7
“xtvkyx” <ljabli@icloud.com>
2025-08-25 16:56:48 +0000

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Print if a random number is positive, zero, or negative
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
