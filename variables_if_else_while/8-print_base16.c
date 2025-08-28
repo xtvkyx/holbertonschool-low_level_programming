2776a7a41ef9b5c2289d42a5c20f4978d080ab46
“xtvkyx” <ljabli@icloud.com>
2025-08-28 12:47:57 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+putchar(i + '0');
+{
+for (i = 'a'; i <= 'f'; i++)
+putchar(i + 'a' - 10);
+}
+putchar('\n');
+return (0);
+}
