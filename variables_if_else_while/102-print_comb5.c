11e55973fa27611df1d6fb59ed53a0f287a712c1
“xtvkyx” <ljabli@icloud.com>
2025-08-29 20:44:56 +0000

    102

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -7,13 +7,12 @@
 int main(void)
 {
 int first, second;
-for (first = 0; first <= 98; first++)
+for (first = 0; first <= 99; first++)
 {
 for (second = first + 1; second <= 98; second++)
 {
 putchar((first / 10) + '0');
 putchar((first % 10) + '0');
-{
 putchar(' ');
 putchar((second / 10) + '0');
 putchar((second % 10) + '0');
@@ -21,7 +20,6 @@ if (first != 98 || second != 99)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
