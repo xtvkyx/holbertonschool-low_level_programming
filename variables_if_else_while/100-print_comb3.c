035f2067cd684609c0ef79588e57ce2c5ab23df4
“xtvkyx” <ljabli@icloud.com>
2025-08-29 12:19:46 +0000

    100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - Prints all unique two-digit combinations in ascending order
  *
  * Return: Always 0 (Success)
  */
@@ -9,15 +9,20 @@
 int main(void)
 {
 int first, second;
-for (first= 0; first <= 9; first++)
+for (first = 0; first <= 9; first++)
 {
 for (second = 0; second <= 9; second++)
 {
-if (second> first)
+if (second > first)
+{
 putchar(first + '0');
 putchar(second + '0');
+if (first !=8 || second !=9)
+{
 putchar(',');
-putchar(' ');                                                      
+putchar(' ');
+}
+}
 }
 }
 putchar('\n');
