028e84749e801dfc62c5c25fc4aea298970106cb
“xtvkyx <ljabli@icloud.com>
2025-08-30 13:01:06 +0000

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+i#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ *
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+  if (n != 98)
+printf("%d, ", n);
+else
+printf("%d\n", n);
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n != 98)
+printf("%d, ", n);
+else
+printf("%d\n", n);
+}
+}
+}
