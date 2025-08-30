ff7e46f07c437b9abccd76f8155536a87f903fb6
“xtvkyx <ljabli@icloud.com>
2025-08-30 12:22:04 +0000

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 int hour, minute;
 for (hour = 0; hour <= 23; hour++)
 {
-for (minute = 0; minute <= 59; hour++)
+for (minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
