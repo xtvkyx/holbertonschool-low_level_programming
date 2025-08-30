8e763b5ae72dd60e39748c620cac3467e0332475
“xtvkyx <ljabli@icloud.com>
2025-08-30 12:04:22 +0000

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the number to check
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
+
