cd7f7fff154d7c09a781c439da7a4c32316feb4e
“xtvkyx <ljabli@icloud.com>
2025-08-30 12:43:13 +0000

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: the sum of x and y
+ */
+int add(int x, int y)
+{
+rint result;
+
+result = x + y;
+
+return (result);
+}
