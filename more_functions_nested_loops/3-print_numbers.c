453c6a4b307e088e3826eb7ac6f4a361120da16f
“xtvkyx” <ljabli@icloud.com>
2025-08-31 18:30:40 +0000

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_numbers - prints the numbers from 0 to 9
  *
@@ -7,6 +6,8 @@
  */
 void print_numbers(void)
 {
-write(1, "0123456789" , 10);
+int i;
+for (i = 0; i <= 9; i++)
+_putchar(i + '0');
 _putchar('\n');
 }
