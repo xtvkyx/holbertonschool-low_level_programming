4c86515c02870a89e85536babc8b2b0a1dd9313c
“xtvkyx” <ljabli@icloud.com>
2025-08-31 17:02:18 +0000

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,12 @@
 /**
  * print_numbers - prints the numbers from 0 to 9
  *
- *Return: nothing
+ * Return: nothing
  */
 void print_numbers(void)
 {
 int i;
 for (i = 0; i <= 9; i++)
-putchar(i + '0');
-putchar('\n');
+_putchar(i + '0');
+_putchar('\n');
 }
