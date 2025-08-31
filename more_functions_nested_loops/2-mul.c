ce5305034961d057e8ba29279dc3ec45cd0cc6e0
“xtvkyx” <ljabli@icloud.com>
2025-08-31 16:46:10 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two integers
+ * @a: first integers
+ * @b: second integers
+ * Return: result of a * b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
