0ea7156776ccac7ab30eee89d7c291edc702081f
“xtvkyx” <ljabli@icloud.com>
2025-09-01 15:42:54 +0000

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - a function that draws a diagonal line in the terminal
+ * @n: number of times the character _ should be printed
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+int i = 0;
+if (n > 0)
+{
+for (; i < n; i++)
+_putchar('_');
+}
+_putchar('\n');
+}
