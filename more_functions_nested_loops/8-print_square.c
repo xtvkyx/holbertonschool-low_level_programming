4eae3a373092c74a6decf98fa8d90881101f8e38
“xtvkyx” <ljabli@icloud.com>
2025-09-01 16:43:11 +0000

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,21 @@
 include "main.h"
 /**
  * print_square - a function that prints a square
- * @n: number of times the character # should be printed
+ * @size: the size of the square
  * Return: nothing
  */
 void print_square(int size)
 {
-
+int i, j;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
