b4e8f32bd95b98d46ac0f926de5e86e08b7165b8
“xtvkyx” <ljabli@icloud.com>
2025-09-01 21:29:54 +0000

    101

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -3,41 +3,46 @@
 static void print_unsigned(unsigned int m);
 
 /**
- * print_number - prints an integer using only _putchar
+ * print_number - prints an integer using _putchar
  * @n: integer to print
  * Return: nothing
  */
 void print_number(int n)
 {
-unsigned int m = (unsigned int)n;
+	unsigned int m = (unsigned int)n;
 
-if (n < 0)
-{
-_putchar('-');
-m = -m; /* safe unsigned negation -> magnitude */
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -m; /* unsigned negation -> magnitude */
+	}
+
+	print_unsigned(m);
 }
-print_unsigned(m);
-}
+
 /**
- * print_unsigned - helper that prints a nonnegative integer using _putchar
+ * print_unsigned - prints a nonnegative integer using _putchar
  * @m: number to print
  * Return: nothing
  */
 static void print_unsigned(unsigned int m)
 {
-unsigned int div = 1;
+	unsigned int div = 1;
 
-if (m == 0)
-{
-_putchar('0');
-return;
+	if (m == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (m / div >= 10)
+		div *= 10;
+
+	while (div > 0)
+	{
+		_putchar('0' + (m / div));
+		m %= div;
+		div /= 10;
+	}
 }
-while (m / div >= 10)
-div *= 10;
-while (div > 0)
-{
-_putchar('0' + (m / div));
-m %= div;
-div /= 10;
-}
-}
+
