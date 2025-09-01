88e4cdd8cdd000573181d2c51ba915379111897f
“xtvkyx” <ljabli@icloud.com>
2025-09-01 14:33:43 +0000

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 #include "main.h"
 /**
- * print_most_numbers - prints the numbers from 0 to 9
+ * print_most_numbers - prints the numbers from 0 to 9, except 2 and 4
  *
  * Return: nothing
  */
@@ -14,4 +14,5 @@ for (i = 0; i <= 9; i++)
 if (i != 2 && i != 4)
 _putchar(i + '0');
 }
+_putchar('\n');
 }
