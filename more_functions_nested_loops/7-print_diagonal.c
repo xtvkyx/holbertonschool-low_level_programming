07a748aa767e8916d2be43439e2b7d4948a52cee
“xtvkyx” <ljabli@icloud.com>
2025-09-01 15:52:16 +0000

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,14 +6,10 @@
  */
 void print_diagonal(int n)
 {
-int i, j = 0;
+int i = 0;
 if (n > 0)
 {
-for (i = 0; i < n; i++)
-_putchar('\');
-}
-{
-for (j = 0; j < i; j++)
+for (; i < n; i++)
 _putchar('\');
 }
 _putchar('\n');
