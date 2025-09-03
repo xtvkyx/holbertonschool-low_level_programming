9467e64004226ca3908a56ab5098a4147f0d9417
“xtvkyx” <ljabli@icloud.com>
2025-09-03 13:15:19 +0000

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * puts - prints a string
+ * @str: an inpur string
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+printf("I do not fear computers. I fear the lack of them - Isaac Asimov.\n");
+}
