59863eb4cc7658b3f8d542ce73a24d73e24bc58b
“xtvkyx” <ljabli@icloud.com>
2025-09-03 12:51:51 +0000

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+int len = 0
+while (s[len] != '\0')
+{
+len++;
+}
+return len;
+}
