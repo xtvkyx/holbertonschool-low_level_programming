0f12fa2095ff94bfa455ffcc2b664b5789ad4486
“xtvkyx” <ljabli@icloud.com>
2025-09-03 12:04:50 +0000

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - takes a pointer to an int as parameter, updates the value points to to 98
  *
  * Return: nothing
  */
 void reset_to_98(int *n)
 {
-	*n = i98;
-	printf("the value is now %d\n", *n);
+*n = 98;
+printf("the value is now %d\n", *n);
 }
