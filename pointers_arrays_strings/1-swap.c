51891b712c96e9d6c6bcd7fe65208ceec88908e8
“xtvkyx” <ljabli@icloud.com>
2025-09-03 12:12:19 +0000

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - a function that swaps the values of two integers
+ * @a: An integer input value
+ * @b: An integer input value
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+int *a = 98, int *b = 42;
+}
