75c43d1082d92ad5514c75b8008123fe1649356b
“xtvkyx” <ljabli@icloud.com>
2025-09-04 12:07:24 +0000

    5

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * a function that prints every other character of a string,
+ * puts2 - function that prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: An input string
  * Return: Nothing
