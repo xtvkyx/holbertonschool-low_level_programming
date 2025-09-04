706a7a31b79c48ebc4fc9abd908b11b362d3bfb1
“xtvkyx” <ljabli@icloud.com>
2025-09-04 10:42:23 +0000

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string.
+ * @s: input string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+    i++;
+}
+}
