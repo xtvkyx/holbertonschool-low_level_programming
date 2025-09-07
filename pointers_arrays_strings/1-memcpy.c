00758607c65c12e6a4726abf838636e3c3ee96c8
“xtvkyx” <ljabli@icloud.com>
2025-09-07 13:38:48 +0000

    1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * @n: number of bytes
+ * @dest: destination to copy
+ * @src: the source to copy from
+ * Return: Returns a pointer to the memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *start = dest;
+{
+while (n--)
+{
+dest = src
+src++
+desr++
+}
+return (*start)
+}
