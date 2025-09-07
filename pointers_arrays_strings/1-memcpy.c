ce9018a13524c37cb735fedb30b35e1fb61c9553
“xtvkyx” <ljabli@icloud.com>
2025-09-07 13:40:32 +0000

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-char *start = dest;
+char *start = *dest;
 {
 while (n--)
 {
-dest = src
-src++
-desr++
+*dest = *src;
+*src++;
+*dest++;
 }
 return (*start)
 }
