a49ad75285d34a128594b49f7780f39ace2224b9
“xtvkyx” <ljabli@icloud.com>
2025-09-07 14:20:14 +0000

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -5,15 +5,13 @@
  * @dest: pointer to destination memory
  * @src: pointer to source memory
  * @n: number of bytes to copy
- *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int i;
+unsigned int i;
 
-        for (i = 0; i < n; i++)
-                dest[i] = src[i];
-
-        return (dest);
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
 }
