4a380e1c0d86c0e9bd1a6d4e7186691658add009
“xtvkyx” <ljabli@icloud.com>
2025-09-07 13:56:34 +0000

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
- * _memcpy - a function that copies memory area
- * @n: number of bytes
- * @dest: destination to copy
- * @src: the source to copy from
- * Return: Returns a pointer to the memory
+ * _memcpy - copies memory area
+ * @dest: pointer to destination memory
+ * @src: pointer to source memory
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-char *start = dest;
-while (n--)
-{
-*dest = *src;
-*dest++;
-*src++;
-}
-return (dest);
+        unsigned int i;
+
+        for (i = 0; i < n; i++)
+                dest[i] = src[i];
+
+        return (dest);
 }
