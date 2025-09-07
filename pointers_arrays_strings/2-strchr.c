64671438b65f2c41a5c8f910abd172db5172f10a
“xtvkyx” <ljabli@icloud.com>
2025-09-07 15:41:30 +0000

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
- * _strchr - a function that locates a character in a string.
+ * _strchr - a function that locates a character in a string
  * @s: an input string to search in
  * @c: an input character to locate into string s
  * Return: returns pointer to c position
- */ 
+ */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+while (*s)
 {
-if (*s == c)
-return (s); 
+if (c == *s)
+return (s);
 s++;
 }
-{
-if (c == '\0')
-return (NULL);
-}
+if (c == *s)
+return (s);
 return (NULL);
 }
