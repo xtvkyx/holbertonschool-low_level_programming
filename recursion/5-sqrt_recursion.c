7fcf1faea240644cdafe2aeb4fbd1e636404858d
“xtvkyx” <ljabli@icloud.com>
2025-09-10 13:28:06 +0000

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,3 +11,17 @@ return (-1);
 
 return (_sqrt(1, n));
 }
+/**
+ * _sqrt - find square root
+ * @prev: previous value
+ * @root: square root value
+ * Return: the square root
+ */
+int _sqrt(int prev, int root)
+{
+if (prev > root)
+return (-1);
+else if (prev * prev == root)
+return (prev);
+return (_sqrt(prev + 1, root));
+}
