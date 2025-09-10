0efbd2f77393c3da66dd9a01e82a8ef9acab92e7
“xtvkyx” <ljabli@icloud.com>
2025-09-10 13:31:37 +0000

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,8 +1,10 @@
 #include "main.h"
+int _sqrt(int prev, int root);
+
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number
- * @n: An input integer
- * Return: the square root of n
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input integer
+ * Return: the square root, or -1 if none
  */
 int _sqrt_recursion(int n)
 {
@@ -12,16 +14,17 @@ return (-1);
 return (_sqrt(1, n));
 }
 /**
- * _sqrt - find square root
- * @prev: previous value
- * @root: square root value
- * Return: the square root
+ * _sqrt - helper to find square root
+ * @prev: current guess
+ * @root: target number
+ * Return: square root, or -1 if not found
  */
 int _sqrt(int prev, int root)
 {
 if (prev > root)
 return (-1);
-else if (prev * prev == root)
+if (prev * prev == root)
 return (prev);
+
 return (_sqrt(prev + 1, root));
 }
