ac9940ae7dfc38af8e1aee973b748fb0f3c127d6
“xtvkyx” <ljabli@icloud.com>
2025-09-10 12:51:12 +0000

    4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - it returns the value of x raised to the power of y.
+ * @x: the base
+ * @y: the exponet
+ * Return: The result of x raised y
+ */
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+return (1);
+else if (y < 0)
+return (-1);
+else if (y == 0)
+return (x);
+return (x *= _pow_recursion(x, y, -1));
+}
