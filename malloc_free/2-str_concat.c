bc2031f073f383f76455f21120cbb1e3f2f6c2d8
“xtvkyx” <ljabli@icloud.com>
2025-09-17 10:37:44 +0000

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to the new string, or NULL if it fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *new_str;
+unsigned int i, j, len1 = 0, len2 = 0;
+if (s1 == NULL)
+s1 = “ “;
+if (s2 == NULL)
+s2 = “ “;
+while (s1[len1] != ‘\0’)
+len1++;
+while (s2[len2] != ‘\0’)
+len2++;
+new_str = malloc((len1 + len2 + 1) * sizeof(char));
+if (new_str == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+new_str[i] = s1[i];
+for (j = 0; j < len2; j++)
+new_str[i + j] = s2[j];
+new_str[i + j] = ‘\0’;
+return (new_str);
+}
