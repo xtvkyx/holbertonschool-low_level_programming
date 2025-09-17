29deb3f897070999260b28834d0029d4dc1c2075
“xtvkyx” <ljabli@icloud.com>
2025-09-17 10:47:09 +0000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ if (s1 == NULL)
 s1 = ““;
 if (s2 == NULL)
 s2 = ““;
-while (s1[len1] != ‘\0’)
+while (s1[len1] != '\0')
 len1++;
-while (s2[len2] != ‘\0’)
+while (s2[len2] != '\0')
 len2++;
 new_str = malloc((len1 + len2 + 1) * sizeof(char));
 if (new_str == NULL)
@@ -26,6 +26,6 @@ for (i = 0; i < len1; i++)
 new_str[i] = s1[i];
 for (j = 0; j < len2; j++)
 new_str[i + j] = s2[j];
-new_str[i + j] = ‘\0’;
+new_str[i + j] = '\0';
 return (new_str);
 }
