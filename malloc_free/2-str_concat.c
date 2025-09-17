e2a27bfe17239bc32aa90623f652e3668bbb161a
“xtvkyx” <ljabli@icloud.com>
2025-09-17 10:50:06 +0000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 char *new_str;
 unsigned int i, j, len1 = 0, len2 = 0;
 if (s1 == NULL)
-s1 = ““;
+s1 = "";
 if (s2 == NULL)
-s2 = ““;
+s2 = "";
 while (s1[len1] != '\0')
 len1++;
 while (s2[len2] != '\0')
