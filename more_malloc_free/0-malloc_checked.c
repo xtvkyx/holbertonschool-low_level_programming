7ab87a6a96184b7c39c4ada19d011ebe88b213c8
“xtvkyx” <ljabli@icloud.com>
2025-09-18 15:48:50 +0000

    R

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: the number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ * if malloc fails, the function terminate the process
+ * with a status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+{
+exit (98);
+}
+return(ptr);
+}
