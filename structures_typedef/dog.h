d8204289badda83abd6c25d811eea8df3577a3a3
“xtvkyx” <ljabli@icloud.com>
2025-09-20 10:23:20 +0000

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * struct dog - a dog's basic info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+char *name;
+float *age;
+char *owner;
+}
