f6fcf2916dbe8fbe9e96cb0408799f060d9a11ff
“xtvkyx” <ljabli@icloud.com>
2025-09-21 15:00:28 +0000

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * print_name - a function that prints a name.
- *
- * @name: pointer to name to print
- * @f: function to print name
+ * print_name - calls function that prints a name.
+ * @name: pointer to the name to print
+ * @f: pointer to a function that prints a name
+ * 
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
 f(name);
 }
