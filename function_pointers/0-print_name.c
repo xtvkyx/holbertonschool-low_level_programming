99abf6718c0fa248206d1ea6576b2edc80b75ee9
“xtvkyx” <ljabli@icloud.com>
2025-09-21 15:08:18 +0000

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,10 @@
-#include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * print_name - calls a function that prints a name
- * @name: pointer to the name to print
- * @f: pointer to a function that prints a name
- *
- * Return: Nothing
- */
+  * print_name - calls function that prints a name
+  * @name: pointer to the name to print
+  * @f: pointer to a function that prints a name
+  * Return: Nothing
+  */
 void print_name(char *name, void (*f)(char *))
 {
 if (name != NULL && f != NULL)
