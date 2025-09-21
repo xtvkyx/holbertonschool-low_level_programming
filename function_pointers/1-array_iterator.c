8495c4a72eab5e949356623aa1072f00fdde444e
“xtvkyx” <ljabli@icloud.com>
2025-09-21 15:55:21 +0000

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,4 +1,4 @@
-u#include "function_pointers.h"
+#include "function_pointers.h"
 /**
  * array_iterator - executes a function on each element of an array
  * @array: pointer to the first element of the array.
