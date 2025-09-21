ca0c0403a6ae2d4b8b2d468fb9db7eb0f0b04e1c
“xtvkyx” <ljabli@icloud.com>
2025-09-21 13:58:04 +0000

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 /**
  * print_name - a function that prints a name.
  *
- * @name
- * @f
+ * @name: pointer to name to print
+ * @f: function to print name
  */
 void print_name(char *name, void (*f)(char *))
 {
-f(name); 
+f(name);
 }
