f1b1dcece5d1bc2e4ba08d2c6067a8c7485fe49d
“xtvkyx” <ljabli@icloud.com>
2025-09-22 12:01:52 +0000

    3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,4 +1,11 @@
 #include "3-calc.h"
+/**
+ * op_add - returns the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
 int op_add(int a, int b)
 {
 return (a + b);
