304e5aa21903c12a941c4950e588a4da25962ae5
“xtvkyx” <ljabli@icloud.com>
2025-09-22 11:59:55 +0000

    3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <string.h>
+
+/**
+ * get_op_func - selects the correct function for the operator
+ * @s: the operator passed as argument
+ *
+ * Return: pointer to the function that corresponds to the operator,
+ * or NULL if no match
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+
+while (ops[i].op != NULL)
+{
+if (strcmp(ops[i].op, s) == 0)
+return (ops[i].f);
+i++;
+}
+return (NULL);
+}
