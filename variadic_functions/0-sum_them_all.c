5a8d4749219138e816076bc3b59fc3f779ee9a83
“xtvkyx” <ljabli@icloud.com>
2025-09-24 13:29:49 +0000

    0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,17 +6,14 @@
  * Return: If n == 0, return 0
  */
 int sum_them_all(const unsigned int n, ...)
-{
-if (n == 0)
-return 0;
 val_list args;
 int sum = 0;
 unsigned int i;
+if (n == 0)
+return (0);
 va_start(args, n);
-for(i = 0; i < n; i++)
-{
+for (i = 0; i < n; i++)
 sum += va_arg(args, int);
-}
 va_end(args);
-return sum;
+return (sum);
 }
