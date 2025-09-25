187179d04e57dba6505bfd2e5c184f277cdeb537
“xtvkyx” <ljabli@icloud.com>
2025-09-25 17:44:07 +0000

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,32 +12,29 @@ unsigned int i = 0;
 char *str, *sep = "";
 
 va_start(args, format);
-
 while (format && format[i])
 {
-if (format[i] == 'c' || format[i] == 'i' ||
-format[i] == 'f' || format[i] == 's')
+switch (format[i])
 {
-printf("%s", sep);
-if (format[i] == 'c')
-printf("%c", va_arg(args, int));
-if (format[i] == 'i')
-printf("%d", va_arg(args, int));
-if (format[i] == 'f')
-printf("%f", va_arg(args, double));
-if (format[i] == 's')
-{
+case 'c':
+printf("%s%c", sep, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(args, double));
+break;
+case 's':
 str = va_arg(args, char *);
 if (str == NULL)
-printf("(nil)");
-else
-printf("%s", str);
+str = "(nil)";
+printf("%s%s", sep, str);
+break;
 }
 sep = ", ";
-}
 i++;
 }
-
 va_end(args);
 printf("\n");
 }
