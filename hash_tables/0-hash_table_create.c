af863e18763e4a3e37aa40aaf05c5ae8689ebb41
“xtvkyx” <ljabli@icloud.com>
2025-10-19 15:11:55 +0000

    0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create - a function that creates a hash table
+ * @size: the number of buckets your hash table will have.
+ * Return: a pointer to the newly created hash table, otherwise return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *table;
+table = malloc(sizeof(hash_table_t));
+if (table == NULL)
+return (NULL);
+table->array = malloc(size * sizeof(hash_node_t *));
+
+if (table -> array == NULL)
+{
+free(table);
+return (NULL);
+}
+for (unsigned long int i = 0; i < size; i++)
+table->array[i] = NULL;
+return (table);
+}
