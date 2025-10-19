6fa72c051bf5b895ab029a136bbd49a60c7102b0
“xtvkyx” <ljabli@icloud.com>
2025-10-19 15:44:30 +0000

    1

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+ * hash_djb2 - a hash function implementing the djb2 algorithm
+ * @str: the input string that you’re turning into a number
+ *
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash = 5381;
+int c;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) + c;
+}
+return (hash);                                                                                                                     }
